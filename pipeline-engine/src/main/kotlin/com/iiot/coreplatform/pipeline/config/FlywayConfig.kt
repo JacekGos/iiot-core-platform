c259f0c7eddf4b058be3a9e0344cc300effdff7a
JacekGos <jacekg93@gmail.com>
2026-04-26 15:05:19 +0200

    KAN-20: fixed ktlint formats

--- pipeline-engine/src/main/kotlin/com/iiot/coreplatform/pipeline/config/FlywayConfig.kt
+++ pipeline-engine/src/main/kotlin/com/iiot/coreplatform/pipeline/config/FlywayConfig.kt
@@ -7,36 +7,34 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class FlywayConfig {
-
     @Bean
     fun postgresFlyway(
         @Value("\${spring.datasource.url}") url: String,
         @Value("\${spring.datasource.username}") username: String,
-        @Value("\${spring.datasource.password}") password: String
-    ): Flyway {
-        return Flyway.configure()
+        @Value("\${spring.datasource.password}") password: String,
+    ): Flyway =
+        Flyway
+            .configure()
             .dataSource(url, username, password)
             .locations("classpath:db/migration")
             .load()
             .also { it.migrate() }
-    }
 
     @Bean
     fun timescaleFlyway(
         @Value("\${spring.timescale.datasource.url}") url: String,
         @Value("\${spring.timescale.datasource.username}") username: String,
-        @Value("\${spring.timescale.datasource.password}") password: String
-    ): Flyway {
-        return Flyway.configure()
+        @Value("\${spring.timescale.datasource.password}") password: String,
+    ): Flyway =
+        Flyway
+            .configure()
             .dataSource(
                 url,
                 username,
-                password
-            )
-            .locations("classpath:db/migration-timescale")
+                password,
+            ).locations("classpath:db/migration-timescale")
             .mixed(true)
             .executeInTransaction(false)
             .load()
             .also { it.migrate() }
-    }
 }
